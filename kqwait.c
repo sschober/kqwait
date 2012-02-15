316078b52fa804a49c8ebc5e5231c1124dc4167a
Sven Schober <sven.schober@uni-ulm.de>
2012-02-15 18:19:24 +0100

    implemented dir scanning

--- kqwait.c
+++ kqwait.c
@@ -39,6 +39,49 @@ void debug(int result, struct kevent* ev){
   }
 }
 
+typedef struct {
+  int count;
+  char** entries;
+} dirInfo;
+
+dirInfo* addEntry( dirInfo* di, char* entry ){
+  int newCount = (NULL != di ? di->count : 0) + 1;
+  dirInfo *newDI = malloc( sizeof(dirInfo) );
+  newDI->entries = malloc( sizeof(char*) * newCount );
+  if(newCount > 1 ){
+    for(int i = 0; i < newCount - 1; i++ ){
+      newDI->entries[i] = di->entries[i];
+    }
+  }
+  newDI->entries[newCount-1] = entry;
+  newDI->count = newCount;
+  return newDI;
+}
+
+void printDirInfo( dirInfo *di ){
+  if( NULL == di ) return;
+  for( int i = 0; i < di->count; i++)
+    fprintf(stderr, "%d: %s\n", i, di->entries[i]);
+}
+
+dirInfo* parseDir(char *filePath) {
+  /* read dir contents */
+  dirInfo *di;
+  fprintf(stderr, "path %s is a directory, reading contents...\n", filePath);
+  DIR* d;
+  if( NULL == (d = opendir(filePath))){
+    perror("fdopendir");
+    exit(EXIT_FAILURE);
+  }
+  struct dirent *dp;
+  while(NULL != (dp = readdir(d))){
+    fprintf(stderr,"%s\n", dp->d_name);
+    di = addEntry(di, dp->d_name);
+  }
+  printDirInfo(di);
+  return di;
+}
+
 int main(int argc, char** argv){
 
   if(2 > argc){
@@ -50,27 +93,28 @@ int main(int argc, char** argv){
 
   struct kevent ev[filesCount];
 
+  struct stat sb;
+
+  //dirInfo *di;
+
   for(int i = 0; i < filesCount; i++){
     char *filePath = argv[i+1];
 
-    int fd = open(filePath, O_RDONLY);
-    if( -1 == fd) {
-      perror("open");
-      return -1;
-    }
+    int fd = -1;
 
-    struct stat sb;
     if( stat( filePath, &sb) == -1 ){
       perror("stat");
       exit(EXIT_FAILURE);
     }
     if( S_ISDIR( sb.st_mode) ){
-      /* read dir contents */
-      fprintf(stderr, "path %s is a directory, reading contents...\n", filePath);
-      DIR* dp;
-      if( ( dp = fdopendir(fd) == NULL ) ){
-	perror("fdopendir");
-	exit(EXIT_FAILURE);
+      parseDir( filePath );
+    }
+
+    if( -1 == fd ){
+      fd = open(filePath, O_RDONLY);
+      if( -1 == fd) {
+	perror("open");
+	return -1;
       }
     }
 
@@ -87,6 +131,9 @@ int main(int argc, char** argv){
   if( result > 0 ){
     fprintf(stdout, "%s\n", (char*) ev[0].udata);
     debug(result, ev);
+    if( S_ISDIR( sb.st_mode ) ){
+      parseDir( (char*) ev[0].udata );
+    }
     return 0;
   }
   else{
