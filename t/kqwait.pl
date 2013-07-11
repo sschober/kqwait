50678723666290efdf0d76b26b8c66b457ffab77
Sven Schober <sven.schober@uni-ulm.de>
2013-07-11 09:44:59 +0200

    Functional testing: waiting on dir
    Refactored testing code a bit

--- t/kqwait.pl
+++ t/kqwait.pl
@@ -3,29 +3,58 @@
 use strict;
 use warnings;
 
-use Test::More tests => 5;
+use Test::More tests => 12;
 
-use IPC::Run qw(run start);
+use IPC::Run qw(run start timeout);
 
-my $test_file = "t/test.txt";
-run("touch $test_file");
+my $test_dir = "t";
+my $test_file = "$test_dir/test.txt";
 my $kqwait_command = "./kqwait";
 
-# wait on single file
-my @kqwait  = split (' ', "$kqwait_command $test_file");
-my $handle = start( \@kqwait, \my( $in,$out,$err));
-run("echo 'hello world' > $test_file");
-ok(finish $handle, 'should return on write');;
-is($err, '', "should be no output to stderr");
-is($out,"$test_file\n", "should return filename on write");
+sub do_test {
+  my ($cmd, $action, $expectation, $msg) = @_;
+  my @kqwait  = split (' ', $cmd);
+  my $handle = start( \@kqwait, \my( $in,$out,$err),timeout(2));
+  run($action);
+  ok(finish $handle, 'should return');;
+  is($err, '', "should produce no output to stderr");
+  is($out,$expectation, $msg);
+}
 
+# wait on single file
+run("touch $test_file");
+do_test(
+  "$kqwait_command $test_file",
+  "echo 'hello world' > $test_file",
+  "$test_file\n",
+  "should return filename on write"
+);
+
+# wait on multiple files
 run("touch $test_file.1");
-@kqwait = split(' ', "$kqwait_command $test_file $test_file.1");
-$handle = start( \@kqwait, \$in,\$out,\$err);
-run("echo 'hello world' > $test_file.1");
-finish $handle;
-is($err, '', "should be no output to stderr");
-is($out,"$test_file.1\n", "should return correct filename when waiting on multiple files");
+do_test(
+  "$kqwait_command $test_file $test_file.1",
+  "echo 'hello world' > $test_file.1",
+  "$test_file.1\n",
+  "should return correct filename when waiting on multiple files"
+);
+
+# wait on directory and create one file
+do_test(
+  "$kqwait_command $test_dir/",
+  "echo 'hello world' > $test_file.2",
+  "+ $test_file.2\n",
+  "should return correct filename when creating a file in dir"
+);
+
+# wait on directory and create one file
+do_test(
+  "$kqwait_command $test_dir/",
+  "rm $test_file",
+  "- $test_file\n",
+  "should return correct filename when deleting a file in dir"
+);
+
 
 run("rm t/test.txt*");
 
